9bcd89a7e1d05d377d991a98590131610eda69cd
Felipe da Silva <felipedasilva.tb.sc@gmail.com>
2021-11-30 23:15:06 -0300

    add get table api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Mutex;
 
-use actix_web::{post, web, App, HttpResponse, HttpServer, Result};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Result};
 
 use table::{CreateTableBody, TableService};
 
@@ -16,6 +16,18 @@ async fn create_table(
     let mut table_service = data.table_service.lock().unwrap();
     let table = table_service.create_table(body.into_inner()).unwrap();
     Ok(HttpResponse::Ok().json(table))
+}
+
+#[get("/table/{table_id}")]
+async fn get_table(
+    data: web::Data<AppState>,
+    web::Path(table_id): web::Path<String>,
+) -> HttpResponse {
+    let table_service = data.table_service.lock().unwrap();
+    match table_service.get_table(table_id) {
+        Ok(table) => HttpResponse::Ok().json(table),
+        Err(_) => HttpResponse::BadRequest().body("Table not found"),
+    }
 }
 
 #[actix_web::main]
@@ -56,6 +68,50 @@ mod tests {
             .uri("/table")
             .set_json(&body)
             .to_request();
+        let result = test::read_response(&mut app, req).await;
+        assert_eq!(
+            result,
+            Bytes::from_static(b"{\"id\":\"test\",\"size_x\":1,\"size_y\":1,\"data\":[[\"\"]]}")
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_table_get_error_not_found() {
+        let service = Mutex::new(TableService::new());
+
+        let mut app = test::init_service(
+            App::new()
+                .data(AppState {
+                    table_service: service,
+                })
+                .service(get_table),
+        )
+        .await;
+        let req = test::TestRequest::get().uri("/table/test").to_request();
+        let result = test::read_response(&mut app, req).await;
+        assert_eq!(result, Bytes::from_static(b"Table not found"));
+    }
+
+    #[actix_rt::test]
+    async fn test_table_get() {
+        let service = Mutex::new(TableService::new());
+
+        let dto = CreateTableBody {
+            id: String::from("test"),
+            size_y: 1,
+            size_x: 1,
+        };
+        service.lock().unwrap().create_table(dto).unwrap();
+
+        let mut app = test::init_service(
+            App::new()
+                .data(AppState {
+                    table_service: service,
+                })
+                .service(get_table),
+        )
+        .await;
+        let req = test::TestRequest::get().uri("/table/test").to_request();
         let result = test::read_response(&mut app, req).await;
         assert_eq!(
             result,
